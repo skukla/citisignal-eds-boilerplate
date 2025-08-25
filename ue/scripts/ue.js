f33b4754b2b769d86fa962918cc02d61e2fcead5
Pankaj Garg <pagarg@adobe.com>
2025-08-25 14:37:00 +0530

    Added carousel related js
    Added carousel related js in ue.js file

--- ue/scripts/ue.js
+++ ue/scripts/ue.js
@@ -9,10 +9,12 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
+
+import { showSlide } from '../../blocks/carousel/carousel.js';
 import { moveInstrumentation } from './ue-utils.js';
 
 const setupObservers = () => {
-  const mutatingBlocks = document.querySelectorAll('div.cards, div.accordion');
+  const mutatingBlocks = document.querySelectorAll('div.cards, div.carousel, div.accordion');
   const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
       if (mutation.type === 'childList' && mutation.target.tagName === 'DIV') {
@@ -55,6 +57,22 @@ const setupObservers = () => {
               moveInstrumentation(removedElements[0].querySelector('div'), addedElements[0].querySelector('summary'));
             }
             break;
+          case 'carousel':
+            if (removedElements.length === 1 && removedElements[0].attributes['data-aue-model']?.value === 'carousel-item') {
+              const resourceAttr = removedElements[0].getAttribute('data-aue-resource');
+              if (resourceAttr) {
+                const itemMatch = resourceAttr.match(/item-(\d+)/);
+                if (itemMatch && itemMatch[1]) {
+                  const slideIndex = parseInt(itemMatch[1], 10);
+                  const slides = mutation.target.querySelectorAll('li.carousel-slide');
+                  const targetSlide = Array.from(slides).find((slide) => parseInt(slide.getAttribute('data-slide-index'), 10) === slideIndex);
+                  if (targetSlide) {
+                    moveInstrumentation(removedElements[0], targetSlide);
+                  }
+                }
+              }
+            }
+            break;
           default:
             break;
         }
@@ -88,6 +106,11 @@ const setupUEEventHandlers = () => {
               details.open = false;
             });
             element.open = true;
+            break;
+          case 'carousel':
+            if (index) {
+              showSlide(blockEl, index);
+            }
             break;
           case 'tabs':
             if (element === block) {
